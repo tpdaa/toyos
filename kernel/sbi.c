ea582fe153b7364ce64be41ada27a35f341a2195
tpdaa <alytpdaa@163.com>
2026-04-03 18:12:55 +0800

    补充注释

--- kernel/sbi.c
+++ kernel/sbi.c
@@ -2,7 +2,7 @@
 
 int sbi_call(uint64 which,uint64 arg0,uint64 arg1,uint64 arg2)
 {
-    register uint64 a0 asm("a0") = arg0;
+    register uint64 a0 asm("a0") = arg0;//定义一个变量 a0，并强制让它绑定到 CPU 的 a0 寄存器，并赋值为 arg0
     register uint64 a1 asm("a1") = arg1;
     register uint64 a2 asm("a2") = arg2;
     register uint64 a7 asm("a7") = which;
